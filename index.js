cd837b37f64332a0d1ed9bebef73b0c61c0b157f
Shivam Kumar <shivamraj6136@gmail.com>
2022-09-13 11:29:08 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app=express();
+const cors= require('cors');
+const data=require('./data.json');
+const port= process.env.PORT || 3000
+app.use(cors());
+app.get('/',(req,resp)=>{
+    resp.send('hello');
+})
+app.get('/data',(req,resp)=>{
+    resp.send(data)
+})
+app.listen(port,()=>{
+    console.log('listening on port 3000')
+})
